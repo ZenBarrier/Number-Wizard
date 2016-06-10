b63c2fd384a28443622c1e2def9ec7c5541e8d44
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-10 16:29:56 -0400

    First Commit
    Created a repo for Number Wizard

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome to number wizard!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
